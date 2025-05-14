d2c39f90b5efff190e540aeee7ef5a875cfba249
leo-garcianie <leogarcianie037@gmail.com>
2025-05-13 21:43:43 -0600

    New route

---
+++ src/app/detail/[id]/page.jsx
@@ -0,0 +1,7 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function FrameworkDetail({ params }) {
+  return <main></main>
+}
