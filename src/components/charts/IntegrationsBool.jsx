9d7ae6b46fbad254d79953855439ef996e091e30
leo-garcianie <leogarcianie037@gmail.com>
2025-05-28 15:25:29 -0600

    Integrations bar boolean

--- src/components/charts/IntegrationsBool.jsx
+++ src/components/charts/IntegrationsBool.jsx
@@ -1,7 +1,7 @@
 export default function IntegrationsBool({ bool }) {
   return (
     <div
-      className={`h-4 w-2/3 rounded-2xl ${
+      className={`h-4 rounded-2xl ${
         bool ? 'bg-[#E6FF79]' : 'border border-[#E6FF79]'
       }`}
     ></div>
