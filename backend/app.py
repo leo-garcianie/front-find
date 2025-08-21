461d8a00f1d0a1ec1f0645471c18d6cfe16cf971
leo-garcianie <leogarcianie037@gmail.com>
2025-08-20 23:44:01 -0600

    Final Backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from backend.routes.api_routes import api_bp
+from routes.api_routes import api_bp
 
 def create_app():
     app = Flask(__name__)
