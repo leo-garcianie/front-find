b7fffc27ee1de2c95ce656f04dcc27e08c2ec121
leo-garcianie <leogarcianie037@gmail.com>
2025-08-20 23:59:47 -0600

    Final Frontend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://front-find-production.up.railway.app/api',
   withCredentials: true,
 });
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <div className="flex w-full justify-center">
       <nav className="fixed left-1/2 top-0 lg:top-8 z-10 -translate-x-1/2 flex w-full max-w-6xl items-center justify-between py-3 px-5 text-[#B4B4B4] bg-white/4 backdrop-blur-md border border-[#999999]/40 rounded-none lg:rounded-2xl">
-        <a href="" className="flex items-center gap-3">
+        <Link to="/" className="flex items-center gap-3">
           <img src='/frontfind-logo.svg' alt='FrontFind Logo' className='size-10'/>
           <span className="text-base lg:text-xl font-bold text-[#B4B4B4]">
             FrontFind
           </span>
-        </a>
+        </Link>
 
         <Link to="/survey" className="green-btn">
           <p className="text-sm md:text-base lg:text-base font-medium">Find</p>
